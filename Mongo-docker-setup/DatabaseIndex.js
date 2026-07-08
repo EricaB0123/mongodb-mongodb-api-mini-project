4b008bc22dc3a107fcd78705e585eacd0ccde1f8
EricaB0123 <erica.li.bradley@gmail.com>
2026-07-08 23:48:40 +1200

    Add comments to clarify recommended index set for user lookups

--- Mongo-docker-setup/DatabaseIndex.js
+++ Mongo-docker-setup/DatabaseIndex.js
@@ -9,3 +9,5 @@ db.getCollection('users').createIndex({ name: 1 }); // 1 for ascending order, -1
 For example, if you want to find all users whose names start with a certain letter, an ascending index will allow you to quickly locate the starting point in the index and then iterate through the results in order. 
 This can be more efficient than searching through an unsorted collection.*/
 
+//Minimal recommended index set for this collection would be to create an index on the 'name' field, as it is likely to be a common search criteria.  And the city field, as it may also be a common search criteria.  This would allow for efficient lookups based on these fields and improve query performance. 
+//  However, the specific index set would depend on the specific use case and query patterns of the application.
